e793904c16001691b0172f0b839b8ed231fda6bb
k-iwao <iwao@hlab.sys.es.osaka-u.ac.jp>
2021-06-04 17:18:47 +0900

    takasu project

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('aaa')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(("abc"))
-    print("abe");
-    print("oooo");
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-print("aaa")
+import itertools
+#関数従属集合の閉包を計算する
+def closure(elems, fds):
+    #２つの引数をset型に変更
+    unused = set(fds)
+    #反射を実行
+    closure = set(elems)
+    length = 0
+    while length != len(closure):
+        length = len(closure)
+        #fdsの要素数でループ
+        for i in range(0, len(fds)):
+            #closureの中にfdsの要素があるかチェック
+            # print(fds[i % len(fds)][0][0], fds[i % len(fds)][1])
+            count = 0
+            for j in range (0, len(fds[i % len(fds)][0])):
+                if fds[i % len(fds)][0][j] in closure:
+                    count += 1
+            if count == len(fds[i % len(fds)][0]):
+                #closureに該当要素の従属部分を追加
+                closure.add(fds[i % len(fds)][1])
+    return closure
+if __name__ == "__main__":
+    lis = ['A','B','C','D','E']
+    f = open('result_closure.txt', 'w')
+    for number in range(1,len(lis)+1):
+        for elems in itertools.combinations(lis, number):
+            fds = [("A","C"),("B","D"),("C","A"),(("A", "B"), "E"),(("A", "D", "E"),"B"),(("B", "E"), "C")]
+            result = closure(elems,fds)
+            for j in range(0, len(elems)):
+                f.write(elems[j])
+            f.write(" -> {")
+            chara = []
+            for i in range(1,len(list(result))+1):
+                chara.append(list(result)[i-1])
+            chara = sorted(chara)
+            for i in range(1,len(chara)):
+                f.write(chara[i-1])
+                f.write(", ")
+            string = [chara[len(chara)-1], "}", '\n']
+            f.writelines(string)
